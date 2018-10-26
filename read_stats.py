6df671ce92ea8593b6c06c990c5b19edd886c2b6
Ricardo Bolio <rbolio90@gmail.com>
2018-10-25 21:09:55 -0500

    add to dropbox

--- read_stats.py
+++ read_stats.py
@@ -1,11 +1,35 @@
 import pandas
 import glob
+import dropbox
+from accounts import token as db_token
 
-stats_file_list = (glob.glob(".\\stats\\*.csv"))
 
-for stat_file in stats_file_list:
-    print stat_file
-    df = pandas.read_csv(stat_file, index_col='DATE', parse_dates=['DATE'])
-    df["CHANGE_IN_FOLLOWERS"] = df['FOLLOWERS'].diff(1)
-    df["CHANGE_IN_FOLLOWING"] = df['FOLLOWING'].diff(1)
-    print(df.tail(5))
+def read_all_csvs():
+    stats_file_list = (glob.glob(".\\stats\\*.csv"))
+    for stat_file in stats_file_list:
+        print stat_file
+        df = pandas.read_csv(stat_file, index_col='DATE', parse_dates=['DATE'])
+        df["CHANGE_IN_FOLLOWERS"] = df['FOLLOWERS'].diff(1)
+        df["CHANGE_IN_FOLLOWING"] = df['FOLLOWING'].diff(1)
+        print(df.tail(5))
+
+
+def read_overall_stats():
+    return pandas.read_csv(".\\stats\\instaPyStats.csv", parse_dates=['DATE'])
+
+
+def to_dropbox(dataframe, path, token):
+    dbx = dropbox.Dropbox(token)
+
+    df_string = dataframe.to_csv(index=False)
+    db_bytes = bytes(df_string)
+    dbx.files_upload(
+        f=db_bytes,
+        path=path,
+        mode=dropbox.files.WriteMode.overwrite
+    )
+
+
+dataframe = read_overall_stats()
+print dataframe
+#to_dropbox(dataframe, "/data.csv", db_token)
