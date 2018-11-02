e20f6df9242ed134185f726f736e5f3242d56343
Ricardo Bolio <rbolio90@gmail.com>
2018-11-02 13:58:59 -0600

    improve readability and upload all csv

--- read_stats.py
+++ read_stats.py
@@ -2,23 +2,16 @@ import pandas
 import glob
 import dropbox
 from accounts import token as db_token
+from os import listdir
+from os.path import isfile, join
+from get_stats import run
 
 
-def read_all_csvs():
-    stats_file_list = (glob.glob(".\\stats\\*.csv"))
-    for stat_file in stats_file_list:
-        print stat_file
-        df = pandas.read_csv(stat_file, index_col='DATE', parse_dates=['DATE'])
-        df["CHANGE_IN_FOLLOWERS"] = df['FOLLOWERS'].diff(1)
-        df["CHANGE_IN_FOLLOWING"] = df['FOLLOWING'].diff(1)
-        print(df.tail(5))
+def read_overall_stats(filename):
+    return pandas.read_csv(".\\stats\\" + filename, parse_dates=['DATE'])
 
 
-def read_overall_stats():
-    return pandas.read_csv(".\\stats\\instaPyStats.csv", parse_dates=['DATE'])
-
-
-def to_dropbox(dataframe, path, token):
+def upload_to_dropbox(dataframe, path, token):
     dbx = dropbox.Dropbox(token)
 
     df_string = dataframe.to_csv(index=False)
@@ -30,6 +23,18 @@ def to_dropbox(dataframe, path, token):
     )
 
 
-dataframe = read_overall_stats()
-print dataframe
-to_dropbox(dataframe, "/data.csv", db_token)
+def all_files_upload():
+    file_data_path = './stats/'
+    file_list = [f for f in listdir(file_data_path) if isfile(join(file_data_path, f))]
+    for individual_file in file_list:
+        upload_path = "/individual_reports/" + individual_file
+        if 'instaPyStats.csv' in individual_file:
+            upload_path = "/" + individual_file
+        if '.csv' in individual_file:
+            data_frame = read_overall_stats(individual_file)
+            upload_to_dropbox(data_frame, upload_path, db_token)
+
+
+if __name__ == '__main__':
+    run()  # runs the 'Get stats' code
+    all_files_upload()
