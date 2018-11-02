7f6c16688896bcfdeb043e2c086e1ed965da44f6
Ricardo Bolio <rbolio90@gmail.com>
2018-11-02 14:12:59 -0600

    no fucking clue

--- read_stats.py
+++ read_stats.py
@@ -1,5 +1,6 @@
+"""Module to read Instagram statistics and upload them to Dropbox for safekeeping"""
+
 import pandas
-import glob
 import dropbox
 from accounts import token as db_token
 from os import listdir
@@ -8,12 +9,13 @@ from get_stats import run
 
 
 def read_overall_stats(filename):
+    """Reads csv file and returns it as a pandas data frame"""
     return pandas.read_csv(".\\stats\\" + filename, parse_dates=['DATE'])
 
 
 def upload_to_dropbox(dataframe, path, token):
+    """uploads pandas dataframe to dropbox as csv"""
     dbx = dropbox.Dropbox(token)
-
     df_string = dataframe.to_csv(index=False)
     db_bytes = bytes(df_string)
     dbx.files_upload(
@@ -36,5 +38,5 @@ def all_files_upload():
 
 
 if __name__ == '__main__':
-    run()  # runs the 'Get stats' code
+    # run()  # runs the 'Get stats' code
     all_files_upload()
